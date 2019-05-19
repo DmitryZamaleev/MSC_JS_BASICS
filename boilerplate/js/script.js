c8f148e141e12cbe1b4c5a6c129e37172b7c9b01
DmitryZamaleev <dzamaleev@gmail.com>
2019-05-19 20:22:51 +0300

    bolier 2

--- boilerplate/js/script.js
+++ boilerplate/js/script.js
@@ -1,1 +1,8 @@
-console.log('Ypu are bitches' + window.location)
+var wallet = prompt('How much money you have?');
+var apple_cnt = prompt('How much apples did you buy');
+var bread_cnt = prompt('How much bread did you buy');
+var apple_price = prompt('Apple price');
+var bread_price = prompt('Bread price');
+var dif= wallet-apple_cnt*apple_price-bread_cnt*bread_price;
+var flag = dif>0;
+document.body.innerHTML='<h1>'+flag+'</h1>'
